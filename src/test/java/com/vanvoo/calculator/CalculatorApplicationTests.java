79344a616615da31efa6c14a698670d681bdeb58
student <linda.vanvooren@ngc.com>
2021-05-15 19:39:15 +0000

    Add logic and test

---
+++ src/test/java/com/vanvoo/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanvoo.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
